c643bde432a6413262c27c60f1bb322c8de17b42
rahulpatil5202 <32543014+rahulpatil5202@users.noreply.github.com>
2018-11-30 17:53:36 +0530

    Chnaged output

--- validateEmails.py
+++ validateEmails.py
@@ -4,7 +4,7 @@ import unicodecsv as csv
 from validate_email import validate_email
 
 
-emails = pd.read_csv('benchmark_nobounce.csv', names=['email_id'], skiprows=1, encoding="ISO-8859-1")
+emails = pd.read_csv('MukulSir.csv', names=['email_id'], encoding="ISO-8859-1")
 
 print("Total records in email input file ", emails.count())
 print("\n\nFirst few records")
@@ -27,10 +27,11 @@ print("Kairee Count ",emails[emails['domain_'].str.contains('kairee')].count())
 print("Aryan Count ",emails[emails['domain_'].str.contains('aibc')].count())
 
 
-validated_emails = open('./benchmark_nobounce_validated.csv','wb')    
+validated_emails = open('./MukulSir_validated.csv','wb')    
 output_writer = csv.writer(validated_emails, delimiter = "|", encoding='latin2')
 print(emails.count())
 itr = 0
+exists = 0
 
 for email in emails.email_id:
     
@@ -46,9 +47,13 @@ for email in emails.email_id:
             valid_address,
             valid_domain, 
             email_exists])
+        if(email_exists == True):
+            exists = exists + 1
+        else:
+            ()
         print(email,"\t"," :Valid Address ",valid_address,"\tValid Domain : ",valid_domain,"\tEmail Exists : ",email_exists)
         itr = itr+1
-        print("\n\nProcessed ",itr," out of ",emails.count())
+        print("\n\nProcessed ",itr," out of ",emails.count()," All OK Count ",exists)
 
     except Exception as e:
         print("\nException occured for email id ",email)
